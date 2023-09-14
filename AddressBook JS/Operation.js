77479af4d6b5519d3f39bdeac29d1c315af4abc4
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-09-14 19:44:59 +0530

    [Nithin]Add: Updation of Adding a contact

--- AddressBook JS/Operation.js
+++ AddressBook JS/Operation.js
@@ -5,8 +5,13 @@ const addressBook = new AddressBook();
 
 const contact1 = new Contact("A", "A", "1", "A", "A", "11111", "1111111111", "a@gmail.com");
 const contact2 = new Contact("B", "B", "2", "B", "B", "22222", "2222222222", "b@gmail.com");
+const contact3 = new Contact("C", "C", "3", "c", "c", "33333", "3333333333", "c@gmail.com");
+
+
 
 addressBook.addContact(contact1);
 addressBook.addContact(contact2);
+addressBook.addContact(contact3);
+
 
 addressBook.displayContacts();
