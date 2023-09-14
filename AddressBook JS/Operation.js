dbf4f5b9e817fda1f08800649103ad998fbc69a5
Takkoli Nithin Kumar Reddy <nithinkumarreddy.t@tvsnext.com>
2023-09-14 19:20:49 +0530

    [Nithin] Add : Updation of Validation using Regex

--- AddressBook JS/Operation.js
+++ AddressBook JS/Operation.js
@@ -3,8 +3,8 @@ const AddressBook = require('./AddressBook');
 
 const addressBook = new AddressBook();
 
-const contact1 = new Contact("A", "A", "123", "A", "A", "11111", "1111111111", "a@gmail.com");
-const contact2 = new Contact("B", "B", "456", "B", "B", "22222", "2222222222", "b@gmail.com");
+const contact1 = new Contact("A", "A", "1", "A", "A", "11111", "1111111111", "a@gmail.com");
+const contact2 = new Contact("B", "B", "2", "B", "B", "22222", "2222222222", "b@gmail.com");
 
 addressBook.addContact(contact1);
 addressBook.addContact(contact2);
